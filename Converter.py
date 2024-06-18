34f982397a1ccb77e777a37e6e0fb9546f908fd5
Anna <94724782+Ministreliya131@users.noreply.github.com>
2024-06-18 13:54:27 +0300

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,217 @@
+import streamlit as st
+import aiohttp
+import asyncio
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from venn import venn
+import os
+from bs4 import BeautifulSoup
+import requests
+import networkx as nx
+import plotly.graph_objs as go
+from PIL import Image
+from streamlit_agraph import agraph, Node, Edge, Config
+
+prot_dict = {}
+prot_gene_dict = {}
+ASSOC_MET_SET = None
+ASSOC_PROT_SET = None
+MET_SET = None
+PROT_SET = None
+
+
+def append_row(df, row):
+    return pd.concat([
+                df,
+                pd.DataFrame([row], columns=row.index)]
+           ).reset_index(drop=True)
+
+
+st.write("# Welcome to PMInteractor!")
+
+st.markdown(
+    """
+        ## PMInteractor is an open-source app framework built for proteometabolomics analyses
+    """
+)
+
+st.markdown(
+    """
+        #### Metabolome* - metabolites associated with proteins\n
+        #### Proteome* - proteins associated with metabolites 
+    """
+)
+
+with st.sidebar:
+    st.write(
+        """
+            Upload your metabolome and/or proteome profiles
+            """
+    )
+
+    proteins = st.text_area("Your protein list (one protein per line)")
+    st.button("Submit", disabled=not proteins, key="protein")
+
+    if proteins is not None:
+        protein_split = proteins.splitlines()
+        if len(protein_split) > 0:
+            PROT_SET = set(protein_split)
+            st.write(len(PROT_SET))
+
+    metabolites = st.text_area("Your metabolite list (one metabolite per line)")
+    st.button("Submit", disabled=not metabolites, key="metabolite")
+
+    if metabolites is not None:
+        metabolite_split = metabolites.splitlines()
+        if len(metabolite_split) > 0:
+            MET_SET = set(metabolite_split)
+            st.write(len(MET_SET))
+
+if st.button("Process your profiles", type="primary"):
+
+    if PROT_SET is not None:
+        with open(
+                '/home/ministreliya131/PycharmProjects/PMInteractor/protein_dictionary_hmdb.csv'
+        ) as p_d:
+            for line in p_d:
+                line_strip = line.strip()
+                key_prot = line_strip.split(",")[0]
+                met_line = line_strip.split(",")[1]
+                if len(met_line) > 0:
+                    met_values = met_line.split("|")
+                else:
+                    met_values = met_line
+                prot_dict.update({key_prot: met_values})
+        st.write("Processing proteome profile...")
+        st.write(f"{len(prot_dict)} proteins were downloaded from HMDB...")
+
+        assoc_met = []
+
+        with open("proteome_to_metabolome_dictionary.csv", "w") as f2:
+            for p in PROT_SET:
+                if p in prot_dict:
+                    assoc_met += prot_dict[p]
+                    print(p, "|".join([x for x in prot_dict[p]]), sep=",", file=f2)
+                else:
+                    print(p, "None", sep=",", file=f2)
+
+        ASSOC_MET_SET = assoc_met
+
+        with open("associated_metabolome.csv", "w") as f3:
+            for m in ASSOC_MET_SET:
+                print(m, file=f3)
+
+        st.write("Finished proteome processing!")
+
+    else:
+        st.write("No proteome profile was given")
+
+    if MET_SET is not None:
+        start_time = time.time()
+
+
+        async def get_task():
+            tasks = []
+            async with aiohttp.ClientSession() as session:
+                for p_hmdb in MET_SET:
+                    task = asyncio.create_task(get_prot_and_gene(session, p_hmdb))
+                    tasks.append(task)
+                    await asyncio.sleep(1)
+                await asyncio.gather(*tasks)
+
+
+        async def get_prot_and_gene(session, p_hmdb):
+            url = f"https://hmdb.ca/metabolites/{p_hmdb}/metabolite_protein_links"
+            async with session.get(url) as response:
+                if response.status == 200:
+                    soup = BeautifulSoup(await response.text(), "html.parser")
+
+                    mdata = []
+                    table = soup.find('table', attrs={"class": "table table-condensed"
+                                                               " table-striped metabolite-protein-links proteins"})
+                    table_body = table.find('tbody')
+                    rows = table_body.find_all('tr')
+                    for row in rows:
+                        cols = row.find_all('td')
+                        cols = [ele.text.strip() for ele in cols]
+                        mdata.append([ele for ele in cols if ele])
+
+                    with open("/home/ministreliya131/PycharmProjects/PMInteractor/tmp", "a") as f:
+                        for i in mdata:
+                            print(p_hmdb, i[2], i[3], sep=",", file=f)
+
+
+        st.write("Processing metabolome profile...")
+
+        asyncio.run(get_task())
+        end_time = time.time() - start_time
+        st.write(f"Finished search with {round(end_time / 60, 2)} minutes!")
+
+        met_dict = pd.read_csv("/home/ministreliya131/PycharmProjects/PMInteractor/tmp", sep=",", header=None)
+        met_dict_g = met_dict.groupby(by=0, as_index=False)[1].agg("|".join)
+        met_dict_g.to_csv("metabolome_to_proteome_dictionary.csv", index=False, header=None)
+
+        assoc_prot = set()
+        with open("associated_proteome.csv", "w") as f4:
+            with open("/home/ministreliya131/PycharmProjects/PMInteractor/tmp") as f:
+                for line in f:
+                    line_s = line.strip().split(",")
+                    assoc_prot.add(line_s[1])
+                    prot_gene_dict.update({line_s[1]: line_s[2]})
+                    print(line_s[1], file=f4)
+
+        ASSOC_PROT_SET = assoc_prot
+
+        # os.remove("/home/ministreliya131/PycharmProjects/PMInteractor/tmp")
+
+        st.write("Finished metabolome processing!")
+
+    else:
+        st.write("No metabolome profile was given")
+
+else:
+    pass
+
+col1, col2 = st.columns([1.5, 1.5])
+
+if ASSOC_MET_SET and ASSOC_PROT_SET is not None:
+    with col1:
+        st.subheader("Metabolome vs Metabolome*")
+        dict_venn_met = {"Metabolome": set(MET_SET), "Metabolome*": set(ASSOC_MET_SET)}
+        cmap_format = ["#00BFFF", "#7B68EE"]
+        venn(dict_venn_met, cmap=cmap_format, fontsize=20, figsize=(8, 8))
+        st.pyplot(plt)
+
+        prot_df = pd.read_csv("proteome_to_metabolome_dictionary.csv", sep=",", header=None)
+        prot_df.rename(columns={0: "Uniprot ID",
+                                1: "Associated metabolites"}, inplace=True)
+        prot_df_single = prot_df[prot_df["Associated metabolites"].isna()]
+        prot_df_multi = prot_df.dropna()
+        prot_df_multi["Metabolite count"] = prot_df_multi["Associated metabolites"].apply(lambda x: len(x.split("|")))
+        prot_df_multi_sort = prot_df_multi.sort_values(by="Metabolite count", ascending=False)
+
+        st.write(f"##### {prot_df_single.shape[0]} proteins have no associations")
+        st.write(f"##### {len(PROT_SET) - prot_df_single.shape[0]} proteins have associations with metabolites")
+        st.table(prot_df_multi_sort[["Uniprot ID", "Metabolite count"]].head(6))
+
+    with col2:
+        st.subheader("Proteome vs Proteome*")
+        dict_venn_prot = {"Proteome": set(PROT_SET), "Proteome*": set(ASSOC_PROT_SET)}
+        cmap_format = ["#DC143C", "#800000"]
+        venn(dict_venn_prot, cmap=cmap_format, fontsize=20, figsize=(8, 8))
+        st.pyplot(plt)
+
+        met_df = pd.read_csv("metabolome_to_proteome_dictionary.csv", sep=",", header=None)
+        met_df.rename(columns={0: "HMDB ID",
+                               1: "Associated proteins"}, inplace=True)
+
+        met_df["Protein count"] = met_df["Associated proteins"].apply(lambda x: len(x.split("|")))
+        met_df_sort = met_df.sort_values(by="Protein count", ascending=False)
+
+        st.write(f"##### {len(MET_SET - set(met_df['HMDB ID'].to_list()))} metabolites have no associations")
+        st.write(f"##### {len(set(met_df['HMDB ID'].to_list()))} metabolites have associations with proteins")
+        st.table(met_df_sort[["HMDB ID", "Protein count"]].head(6))
+
+else:
+    pass
---
+++ Interactor.py
@@ -0,0 +1,188 @@
+import streamlit as st
+import pandas as pd
+import os
+import requests
+from streamlit_agraph import agraph, Node, Edge, Config
+import json
+
+PROT_LIST = []
+MET_LIST = []
+ASSOC_MET_LIST = []
+prot_gene_dict = {}
+
+
+def append_row(df, row):
+    return pd.concat([
+        df,
+        pd.DataFrame([row], columns=row.index)]
+    ).reset_index(drop=True)
+
+
+def get_key_by_value(dict, search):
+    for name, val in dict.items():
+        if val == search:
+            return name
+
+
+st.write("# Welcome to PMInteractor!")
+
+with open("/home/ministreliya131/PycharmProjects/PMInteractor/gene_prot_dict.tsv") as ff:
+    for line in ff:
+        l_s = line.strip().split("\t")
+        prot_gene_dict.update({l_s[0]: l_s[1]})
+
+with open("/home/ministreliya131/PycharmProjects/PMInteractor/proteome_to_metabolome_dictionary.csv") as pm:
+    for line in pm:
+        ll = line.strip().split(",")
+        PROT_LIST.append(ll[0])
+        if ll[1] != "None":
+            for j in ll[1].split("|"):
+                ASSOC_MET_LIST.append(j)
+
+with open("/home/ministreliya131/PycharmProjects/PMInteractor/metabolome_to_proteome_dictionary.csv") as mp:
+    for line in mp:
+        ll = line.strip().split(",")
+        MET_LIST.append(ll[0])
+
+gene_to_string = []
+err_lst = []
+for p in PROT_LIST:
+    try:
+        gene_to_string.append(prot_gene_dict[p])
+    except KeyError:
+        err_lst.append(p)
+
+# string_api_url = "https://version-11-5.string-db.org/api"
+# output_format = "json"
+# method = "enrichment"
+#
+# request_url = "/".join([string_api_url, output_format, method])
+#
+# params = {
+#     "identifiers": "%0d".join(gene_to_string),
+#     "species": 9606,
+#     "caller_identity": "PMInteractor"
+# }
+#
+# response = requests.post(request_url, data=params)
+#
+# data = json.loads(response.text)
+#
+# func_dict = pd.DataFrame(columns=["Term", "Protein List",
+#                                   "Category", "Description", "FDR"])
+# for row in data:
+#
+#     term = row["term"]
+#     preferred_names = ",".join(row["preferredNames"])
+#     fdr = float(row["fdr"])
+#     description = row["description"]
+#     category = row["category"]
+#     my_input = str(row["number_of_genes"])
+#     bkg = str(row["number_of_genes_in_background"])
+#
+#     if fdr < 0.01:
+#         # st.write("\t".join([term, category, my_input,
+#         #                    bkg, preferred_names, str(fdr), description]))
+#         new_row = pd.Series({"Term": term, "Protein List": preferred_names,
+#                              "Category": category.upper(), "Description": description,
+#                              "FDR": fdr})
+#         func_dict = append_row(func_dict, new_row)
+#
+# options_1 = st.multiselect(
+#     "Choose categories",
+#     list(func_dict["Category"].unique()),
+#     list(func_dict["Category"].unique())[0])
+#
+# opt1_df = pd.DataFrame({"Category": options_1})
+# for_inter_1 = pd.merge(func_dict, opt1_df, left_on="Category", right_on="Category", how="inner")
+# for_inter_1 = for_inter_1[["Term", "Protein List", "Category", "Description", "FDR"]]
+#
+# options_2: object = st.multiselect(
+#     "Choose pathways/functions/compartments",
+#     list(for_inter_1["Description"].unique()),
+#     list(for_inter_1["Description"].unique())[0])
+#
+# opt2_df = pd.DataFrame({"Description": options_2})
+# for_inter_2 = pd.merge(for_inter_1, opt2_df, left_on="Description", right_on="Description", how="inner")
+# for_inter_2 = for_inter_2[["Term", "Protein List", "Category", "Description", "FDR"]]
+#
+# pre_prot_list = []
+# for pl in for_inter_2["Protein List"]:
+#     pre_prot_list += pl.split(",")
+# prot_lst = list(set(pre_prot_list))
+#
+# plst = [get_key_by_value(prot_gene_dict, p) for p in prot_lst]
+
+string_api_url_2 = "https://version-12-0.string-db.org/api"
+output_format_2 = "tsv-no-header"
+method_2 = "network"
+
+request_url_2 = "/".join([string_api_url_2, output_format_2, method_2])
+
+params_2 = {
+    "identifiers": "%0d".join(PROT_LIST),
+    "species": 9606,
+    "caller_identity": "PMInteractor",
+    "network_type": "physical"
+}
+
+response_2 = requests.post(request_url_2, data=params_2)
+
+# st.write("got response")
+
+met_prot_inter = pd.DataFrame(columns=['Source', 'Target', 'Score'])
+met_prot_d = pd.read_csv("/home/ministreliya131/PycharmProjects/PMInteractor/proteome_to_metabolome_dictionary.csv",
+                         sep=",", header=None)
+for index, line in met_prot_d.iterrows():
+    if line[0] in PROT_LIST:
+        if str(line[1]) == 'nan':
+            continue
+        else:
+            for s_line in line[1].split("|"):
+                if s_line in MET_LIST:
+                    new_row = pd.Series({'Source': prot_gene_dict[line[0]], 'Target': s_line, 'Score': 1.0})
+                    met_prot_inter = append_row(met_prot_inter, new_row)
+
+df_inter = pd.DataFrame(columns=['Source', 'Target', 'Score'])
+for line in response_2.text.strip().split("\n"):
+    l = line.strip().split("\t")
+    p1, p2 = l[2], l[3]
+
+    experimental_score = float(l[5])
+    if experimental_score >= 0.9:
+        new_row = pd.Series({'Source': p1, 'Target': p2, 'Score': experimental_score})
+        df_inter = append_row(df_inter, new_row)
+
+df_i = pd.concat([met_prot_inter, df_inter], ignore_index=True)
+pre_nodes_l = set(list(df_i["Source"]) + list(df_i["Target"]))
+
+nodes = []
+edges = []
+for i in pre_nodes_l:
+    if "HMDB" in i:
+        nodes.append(Node(id=i,
+                          label=i,
+                          size=25,
+                          color="#0000CD",
+                          shape="square"))
+    else:
+        nodes.append(Node(id=i,
+                          label=i,
+                          size=25,
+                          color="#DC143C",
+                          shape="dot"))
+for i, lines in df_i.iterrows():
+    edges.append(Edge(source=lines[0],
+                      target=lines[1]))
+
+config = Config(width=650,
+                height=850,
+                directed=False,
+                physics=False,
+                hierarchical=False,
+                # **kwargs
+                )
+
+return_value = agraph(nodes=nodes,
+                      edges=edges,
+                      config=config)
